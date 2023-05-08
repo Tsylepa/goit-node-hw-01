b3b1cbf52ca9f790cb116004d0f33fb9b45282ca
tsylepa <tsylepa@gmail.com>
2023-05-08 16:03:33 +0100

    Tasks completed

---
+++ README.md
@@ -0,0 +1,11 @@
+List:
+https://ibb.co/Z64GDhd
+
+Add:
+https://ibb.co/QkP15t7
+
+Get:
+https://ibb.co/2Z0C6Ff
+
+Remove:
+https://ibb.co/2gBv4Jh
---
+++ index.js
@@ -0,0 +1,42 @@
+const contactService = require("./contacts");
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const allContacts = await contactService.listContacts();
+      return console.table(allContacts);
+
+    case "get":
+      const contactById = await contactService.getContactById(id);
+      return console.log(contactById);
+
+    case "add":
+      const newContact = await contactService.addContact({
+        name,
+        email,
+        phone,
+      });
+      return console.log(newContact);
+
+    case "remove":
+      const removeContact = await contactService.removeContact(id);
+      return console.log(removeContact);
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
